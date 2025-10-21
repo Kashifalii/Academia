2f23bc44db0539452dd802d7701c7ef68dc408e5
Kashif Ali <alyyk82@gmail.com>
2025-10-21 05:30:58 +0500

    homepage progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,27 @@
-"use client"
-import Card from "@/components/card";
+"use client";
 import Navbar from "@/components/navbar";
 import React from "react";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 export default function Home() {
-
-  const cardDetails = [
-    {
-      logo: "mkcdcmdk",
-      title: "Kitchen Foil",
-      paragraph: "Perfect for cooking, wrapping, and food preservation."
-    },
-    {
-      logo: "mkcdcmdk",
-      title: "Kitchen Foil",
-      paragraph: "Perfect for cooking, wrapping, and food preservation."
-    },
-    {
-      logo: "mkcdcmdk",
-      title: "Kitchen Foil",
-      paragraph: "Perfect for cooking, wrapping, and food preservation."
-    }
-  ]
-
   return (
     <>
-      <Navbar />
-      <section className="text-2xl flex justify-between">
-        {cardDetails.map((item, index) => <Card key={index} title={item.title} paragraph={item.paragraph} />)}
-      </section>
+      <header className="bg-[url('../../public/Images/bg_1.jpg')] bg-cover h-screen w-full flex items-center ">
+        <Navbar />
+        <div className="container mx-auto px-4 relative">
+
+          {/* header-content */}
+          <div className="text-box w-full md:w-[500px]">
+            <h6 className="text-gray-100 uppercase font-bold mb-1 text-sm xs:text-base">Welcome to Academia</h6>
+            <h1 className="text-6xl xxs:text-4xl md:text-5xl text-white font-bold mb-4">Best Online Education Expertise</h1>
+            <p className="text-gray-50 text-lg font-medium leading-snug mb-10">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <div className="button-wrapper flex-col xs:flex xs:flex-row xs:items-center gap-3 w-full xs:w-fit ">
+              <a href="#" className="bg-myblue text-white px-7 py-4 mb-3 xs:mb-0 text-sm font-bold rounded-sm flex items-center xs:justify-baseline justify-center gap-2 xs:w-fit w-full transition-all hover:shadow-lg">GET STARTED NOW <MdOutlineArrowRightAlt className="text-xl inline" /> </a>
+              <a href="#" className="bg-white text-myblue px-7 py-4 text-sm font-bold rounded-sm flex items-center xs:justify-baseline justify-center gap-2 xs:w-fit w-full transition-all hover:shadow-lg ">VIEW COURSE <MdOutlineArrowRightAlt className="text-xl inline" /></a>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,116 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 export default function Navbar() {
+  const [openNav, setOpenNav] = useState(false);
+  const pathname = usePathname();
 
-    const [openNav, setOpenNav] = useState(false)
-    function toggleNavbar() {
-        setOpenNav(!openNav)
-    }
+  function toggleNavbar() {
+    setOpenNav(!openNav);
+  }
 
-    return (
-        <nav className="bg-white shadow-md py-2 relative">
-            <div className="container mx-auto px-4 bg-amber-100">
-                <button className="bg-amber-200 rounded p-5" onClick={toggleNavbar}>Click</button>
-                <Link href="/about" className="">cdcdcdc</Link>
-                <div className={`box w-full h-[200px] bg-amber-300 absolute bottom-[-200px] ${openNav ? "block" : "hidden"}`}></div>
-            </div>
-        </nav>
-    );
+  // Helper function to check active link
+  const isActive = (path: string) =>
+    pathname === path
+      ? "text-myblue font-bold"
+      : "text-white hover:text-myblue transition";
+
+  return (
+    <nav className="absolute top-0 w-full">
+      <div className="container mx-auto px-4 relative">
+        {/* upper */}
+        <div className="py-4 flex items-center justify-between">
+          {/* logo */}
+          <a href="#" className="flex flex-col gap-0 text-white">
+            <span className="text-3xl font-extrabold mb-1">ACADEMIA</span>
+            <span className="text-[13px] font-bold leading-0 ">
+              ONLINE EDUCATION & LEARNING
+            </span>
+          </a>
+          {/* social links */}
+          <div className="flex items-center gap-3">
+            <a
+              href="#"
+              className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-[#1eb2a675] hover:bg-myblue hover:shadow-lg text-white transition-all "
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="#"
+              className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-[#1eb2a675] hover:bg-myblue hover:shadow-lg text-white transition-all "
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="#"
+              className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-[#1eb2a675] hover:bg-myblue hover:shadow-lg text-white transition-all "
+            >
+              <GrInstagram />
+            </a>
+            <a
+              href="#"
+              className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-[#1eb2a675] hover:bg-myblue hover:shadow-lg text-white transition-all "
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+
+        {/* lower */}
+        <div className="bg-[#fcfcfc28] backdrop-blur-xs flex items-center justify-between">
+          <ul className="flex items-center gap-9 ps-3 py-3 text-[17px] font-semibold">
+            <li>
+              <Link href="/" className={isActive("/")}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/courses" className={isActive("/courses")}>
+                All Courses
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className={isActive("/about")}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/team" className={isActive("/team")}>
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link href="/pricing" className={isActive("/pricing")}>
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link href="/journal" className={isActive("/journal")}>
+                Journal
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className={isActive("/contact")}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {/* get certified btn */}
+          <a
+            href="#"
+            className="bg-myblue hover:bg-[#1b9c92] px-16 py-7 text-sm text-white font-bold tracking-wider transition-all"
+          >
+            GET CERTIFIED
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
