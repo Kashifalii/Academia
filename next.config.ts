71a1b7eec9c9b9e09282dc9e6c9b8d4ae866fb94
Kashif Ali <alyyk82@gmail.com>
2025-11-08 00:31:14 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
