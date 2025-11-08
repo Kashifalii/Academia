3035ded65faadb0f43ccd53eefa2f9790a21580c
Kashif Ali <alyyk82@gmail.com>
2025-11-08 18:15:59 +0500

    updated

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -103,13 +103,13 @@ export default function Testimonial() {
                       height={80}
                     />
                     <div>
-                      <h4
+                      <h3
                         className={`text-xl font-bold uppercase mb-2 ${
                           isActive ? "text-white" : "text-myblack"
                         }`}
                       >
                         {data.userName}
-                      </h4>
+                      </h3>
                       <p
                         className={`${
                           isActive ? "text-white/90" : "text-gray-500"
